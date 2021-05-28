4c512c07ca38e5baf4861c0770c6394cf393bf47
iKubernetes <mage@magedu.com>
2021-05-28 16:09:17 +0800

    update priority levels

---
+++ Cluster-Manager/weighted-rr/README.md
@@ -0,0 +1,31 @@
+# Outlier Detection Demo
+
+### 环境说明
+五个Service:
+- envoy：Front Proxy,地址为172.31.27.2
+- webserver01：第一个后端服务
+- webserver01-sidecar：第一个后端服务的Sidecar Proxy,地址为172.31.27.11
+- webserver02：第二个后端服务
+- webserver02-sidecar：第二个后端服务的Sidecar Proxy,地址为172.31.27.12
+- webserver03：第三个后端服务
+- webserver03-sidecar：第三个后端服务的Sidecar Proxy,地址为172.31.27.13
+
+### 运行和测试
+1. 创建
+```
+docker-compose up
+```
+
+2. 测试
+```
+# 使用如下脚本即可直接发起服务请求，并根据结果中统计的各后端端点的响应大体比例，判定其是否能够大体符合加权最少连接的调度机制；
+./send-request.sh 172.31.27.2
+```
+
+3. 停止后清理
+```
+docker-compose down
+```
+
+## 版权声明
+本文档版本归[马哥教育](www.magedu.com)所有，未经允许，不得随意转载和商用。
--- Cluster-Manager/weighted-rr/send-request.sh
+++ Cluster-Manager/weighted-rr/send-request.sh
@@ -6,11 +6,16 @@ declare -i green=0
 #interval="0.1"
 counts=300
 
+echo "Send 300 requests, and print the result. This will take a while."
+echo ""
+echo "Weight of all endpoints:"
+echo "Red:Blue:Green = 1:3:5"
+
 for ((i=1; i<=${counts}; i++)); do
-	if curl -s http://$1/service/colors | grep "red" &> /dev/null; then
+	if curl -s http://$1/hostname | grep "red" &> /dev/null; then
 		# $1 is the host address of the front-envoy.
 		red=$[$red+1]
-	elif curl -s http://$1/service/colors | grep "blue" &> /dev/null; then
+	elif curl -s http://$1/hostname | grep "blue" &> /dev/null; then
 		blue=$[$blue+1]
 	else
 		green=$[$green+1]
@@ -18,4 +23,6 @@ for ((i=1; i<=${counts}; i++)); do
 #	sleep $interval
 done
 
+echo ""
+echo "Response from:"
 echo "Red:Blue:Green = $red:$blue:$green"
